1cc7267ca2c7c3b1fc198f8b028eab97204c702e
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-19 01:16:55 +0300

    add configs

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+"""Flask Configuration"""
 import os
 
 path_file= os.path.abspath(os.getcwd()) + "/db/userprofile.db"
@@ -7,10 +8,18 @@ class Config:
     DEVELOPMENT=False
     DEBUG=False
     TESTING=False
-    SQLALCHEMY_DATABASE_URI =""
+    SQLALCHEMY_DATABASE_URI ="sqlite:///userprofile.db"
     SQLALCHEMY_TRACK_MODIFICATIONS= True
 
-class DevConfig(Config):
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///userprofile.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class DevelopmentConfig(Config):
      DEVELOPMENT=True
      DEBUG=True
      SQLALCHEMY_DATABASE_URI =f"sqlite:///{path_file}"
+class TestingConfig(DevelopmentConfig):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_userprofile.db'
