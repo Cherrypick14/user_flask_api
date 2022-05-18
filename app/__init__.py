30dfce9a9016ec6e913c27a5dd5c5122a66817d1
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-19 01:14:53 +0300

    change to development.config

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import JWTManager
 
 
 app= Flask(__name__)
-app.config.from_object("config.DevConfig")
+app.config.from_object("config.DevelopmentConfig")
 app.config["JWT-SECRET-KEY"]= "B89A2132CA377D91"
 jwt= JWTManager(app)
 
