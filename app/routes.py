0a2288534bac37e8b4748d0c7be9ae03d3996e33
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-19 01:18:38 +0300

    no change

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import unset_jwt_cookies
 
-@app.route("/registers", methods=["POST"])
+@app.route("/register", methods=["POST"])
 def register():
     data= request.json
 
